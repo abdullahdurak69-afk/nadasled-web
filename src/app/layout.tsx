cd6ff29b6ed8087bd1ffbd9a5a22ca44c4e4f4a3
Abdullah Durak <abdullahdurak@192.168.1.103>
2026-04-22 21:52:04 +0300

    feat: Google Analytics 4 (G-4VYW0MWY61) entegrasyonu eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,32 +24,23 @@ export const metadata: Metadata = {
   robots: { index: true, follow: true },
 };
 
-// TODO: GTM ID'nizi buraya girin
-const GTM_ID = "GTM-XXXXXXX";
+const GA_ID = "G-4VYW0MWY61";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
       <head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_ID}');`,
           }}
         />
       </head>
       <body className={`${inter.className} min-h-full flex flex-col`}>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
